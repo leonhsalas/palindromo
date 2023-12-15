d4d1f0997803b8f88596cb3102cbc94991251e52
Leon Nicolhas <leonnhsalas@gmail.com>
2023-12-15 09:06:43 -0600

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-palabra = input("Cual va a ser la palabra")
+palabra = input("Cual va a ser la palabra ")
+
+palindromo = palabra[::-1]
+if palabra == '':
+    print("Escribe algo por favor")
+elif palabra == palindromo:
+    print("Es un palindromo")
+else:
+    print("No es un palindromo")
